c72be29810ac64b2f2b7688d8a02ccdd21ec2a8c
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-01 16:52:18 +0800

    feat: init prisma as ORM

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,20 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 exports.indexGet = (req, res) => {
   res.render("index", {
     title: "Strive",
   });
+};
+
+exports.indexPost = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await prisma.users.create({
+    data: {
+      username: username,
+      password: password,
+    },
+  });
+  console.log(user);
+  res.redirect("/");
 };
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,5 +3,6 @@ const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
 indexRouter.get("/", indexController.indexGet);
+indexRouter.post("/", indexController.indexPost);
 
 module.exports = indexRouter;
