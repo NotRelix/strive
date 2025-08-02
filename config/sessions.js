418bc94bd90d9f0652db91744bd90b48e854e344
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-02 10:14:01 +0800

    feat: store sessions using Prisma

--- config/sessions.js
+++ config/sessions.js
@@ -1,3 +1,5 @@
+const { PrismaClient } = require("@prisma/client");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const session = require("express-session");
 require("dotenv").config();
 
@@ -5,6 +7,11 @@ const sessionsMiddleware = session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  store: new PrismaSessionStore(new PrismaClient(), {
+    checkPeriod: 1000 * 60 * 2,
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined,
+  }),
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
   },
