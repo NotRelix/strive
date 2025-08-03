a66bf5c6301b8df2f2fd049fa7f1d0d8bd91115d
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-03 16:15:35 +0800

    style: add content and style to add file modal

--- public/js/addFileModal.js
+++ public/js/addFileModal.js
@@ -1,12 +1,13 @@
 const addFileBtn = document.querySelector(".add-file-btn");
 const addFileModal = document.querySelector(".add-file-modal");
+const addFileCloseBtn = addFileModal.querySelector(".modal-close");
 
 addFileBtn.addEventListener("click", () => {
   addFileModal.classList.add("show");
 });
 
 document.addEventListener("click", (e) => {
-  if (e.target === addFileModal) {
+  if (e.target === addFileModal || e.target === addFileCloseBtn) {
     addFileModal.classList.remove("show");
   }
 });
