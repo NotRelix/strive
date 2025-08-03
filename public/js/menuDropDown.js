f9446c45b99821a2175c86bb698b563826ea161c
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-03 08:57:29 +0800

    style: rework navbar to be simpler

--- public/js/menuDropDown.js
+++
@@ -1,9 +0,0 @@
-const menuLinks = document.querySelector(".menu-links");
-
-document.addEventListener("click", (e) => {
-  if (e.target.closest(".menu-container")) {
-    menuLinks.classList.toggle("hidden");
-  } else {
-    menuLinks.classList.add("hidden");
-  }
-});
