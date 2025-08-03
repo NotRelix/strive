aee8dc560954b97b245cc6fa8cc1141fef43e8ad
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-03 08:49:45 +0800

    feat: add GET /folders route for new home page

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -5,6 +5,7 @@ const registerRouter = require("./registerRouter");
 const loginRouter = require("./loginRouter");
 const logoutRouter = require("./logoutRouter");
 const fileRouter = require("./fileRouter");
+const folderRouter = require("./folderRouter");
 
 indexRouter.get("/", indexController.indexGet);
 
@@ -12,5 +13,6 @@ indexRouter.use("/login", loginRouter);
 indexRouter.use("/register", registerRouter);
 indexRouter.use("/logout", logoutRouter);
 indexRouter.use("/file", fileRouter);
+indexRouter.use("/folders", folderRouter);
 
 module.exports = indexRouter;
