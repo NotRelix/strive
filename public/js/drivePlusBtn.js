154acaa14f712ca10640d46c937278fe34136b08
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-04 16:02:59 +0800

    feat: add animation to menu popup in home page

---
+++ public/js/drivePlusBtn.js
@@ -0,0 +1,12 @@
+const plusBtn = document.querySelector(".plus-btn-container");
+const plusBtnMenu = document.querySelector(".plus-btn-menu-container");
+
+plusBtn.addEventListener("click", () => {
+  plusBtnMenu.classList.add("show");
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".plus-btn-container")) {
+    plusBtnMenu.classList.remove("show");
+  }
+});
