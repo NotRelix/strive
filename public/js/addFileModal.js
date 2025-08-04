e937e48c28a456fc79d55ea39bea9ebec63993ea
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-04 23:14:38 +0800

    fix: add animation to modal on upload file

--- public/js/addFileModal.js
+++ public/js/addFileModal.js
@@ -1,11 +1,17 @@
 const addFileBtn = document.querySelector(".add-file-btn");
 const addFileModal = document.querySelector(".add-file-modal");
+const addFileForm = document.querySelector(".add-file-form");
 const addFileCloseBtn = addFileModal.querySelector(".modal-close");
 const backdrop = document.querySelector(".backdrop");
 
 addFileBtn.addEventListener("click", () => {
   addFileModal.classList.add("show");
   backdrop.classList.add("show");
+});
+
+addFileForm.addEventListener("submit", () => {
+  addFileModal.classList.remove("show");
+  backdrop.classList.remove("show");
 });
 
 document.addEventListener("click", (e) => {
