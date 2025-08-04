7b67a9ace9ff50ef6a86c23485dadb6ffac260a1
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-04 16:34:34 +0800

    feat: show 'add file' on mobile devices with transition

--- public/js/drivePlusBtn.js
+++ public/js/drivePlusBtn.js
@@ -1,5 +1,7 @@
 const plusBtn = document.querySelector(".plus-btn-container");
 const plusBtnMenu = document.querySelector(".plus-btn-menu-container");
+const plusBtnMenuContent = document.querySelector(".plus-btn-menu-content");
+const plusAddFileBtn = document.querySelector(".plus-add-file-btn");
 
 plusBtn.addEventListener("click", () => {
   plusBtnMenu.classList.add("show");
@@ -7,7 +9,11 @@ plusBtn.addEventListener("click", () => {
 });
 
 document.addEventListener("click", (e) => {
-  if (!e.target.closest(".plus-btn-container")) {
+  if (!e.target.closest(".plus-btn-container") && e.target !== plusBtnMenuContent) {
     plusBtnMenu.classList.remove("show");
   }
 });
+
+plusAddFileBtn.addEventListener("click", () => {
+  addFileModal.classList.add("show");
+})
