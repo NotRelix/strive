94f7914d307db3e798f5d7c9e17235313f070569
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-04 17:09:17 +0800

    feat: enhance animation on modals and popups

--- public/js/drivePlusBtn.js
+++ public/js/drivePlusBtn.js
@@ -4,13 +4,13 @@ const plusBtnMenuContent = document.querySelector(".plus-btn-menu-content");
 const plusAddFileBtn = document.querySelector(".plus-add-file-btn");
 
 plusBtn.addEventListener("click", () => {
-  plusBtnMenu.classList.add("show");
+  plusBtnMenu.classList.add("show-content");
   backdrop.classList.add("show");
 });
 
 document.addEventListener("click", (e) => {
   if (!e.target.closest(".plus-btn-container") && e.target !== plusBtnMenuContent) {
-    plusBtnMenu.classList.remove("show");
+    plusBtnMenu.classList.remove("show-content");
   }
 });
 
